c588a829b10472eac669c791c584dc13d45a18b7
Artnalyze <artnalyze@gmail.com>
2020-07-24 21:48:29 +0700

    extract widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,38 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(
-        title: Text("Home"),
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          // backgroundColor: Colors.blueAccent,
+          // backgroundColor: Color.fromARGB(0xff, 230, 300, 390),
+          // backgroundColor: Color.fromRGBO(230, 230, 290, 0.5),
+          backgroundColor: Color(0xffE60FAD),
+          leading: Icon(Icons.home),
+          title: Text("Test"),
+          actions: <Widget>[
+            IconButton(
+                icon: Icon(Icons.notifications),
+                onPressed: () {
+                  print('Your click notification');
+                }),
+          ],
+        ),
+        body: Center(
+          child: Text("Hello Flutter"),
+        ),
       ),
-      body: Center(
-        child: Text("Hello Flutter"),
-      ),
-    ),
-  ));
+    );
+  }
 }
