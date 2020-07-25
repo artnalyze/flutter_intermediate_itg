dc50bddd72761e95038486a6a08c4bebaff2a9da
Artnalyze <artnalyze@gmail.com>
2020-07-25 20:04:17 +0700

    mediaquery and media layout

--- lib/routers.dart
+++ lib/routers.dart
@@ -1,7 +1,11 @@
 // import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_stock/screens/home/home_screen.dart';
+import 'package:flutter_stock/screens/media_query/media_layout.dart';
+import 'package:flutter_stock/screens/media_query/media_query.dart';
 
 final Map<String, WidgetBuilder> routes = <String, WidgetBuilder>{
-  "/": (BuildContext context) => MyHomePage()
+  // "/": (BuildContext context) => MyHomePage(),
+  // "/": (BuildContext context) => MediaQueryScreen(),
+  "/": (BuildContext context) => MediaLayoutScreen(),
 };
