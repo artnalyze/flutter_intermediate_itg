830d3ced6216ce881e4abc17e7cd163505a2579f
Artnalyze <artnalyze@gmail.com>
2020-07-25 19:12:09 +0700

    split code

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_stock/screens/home/home_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -13,40 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: MyScaffold(),
-    );
-  }
-}
-
-class MyScaffold extends StatefulWidget {
-  MyScaffold({Key key}) : super(key: key);
-
-  @override
-  _MyScaffoldState createState() => _MyScaffoldState();
-}
-
-class _MyScaffoldState extends State<MyScaffold> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // backgroundColor: Colors.blueAccent,
-        // backgroundColor: Color.fromARGB(0xff, 230, 300, 390),
-        // backgroundColor: Color.fromRGBO(230, 230, 290, 0.5),
-        backgroundColor: Color(0xffE60FAD),
-        leading: Icon(Icons.home),
-        title: Text("Master Flutter"),
-        actions: <Widget>[
-          IconButton(
-              icon: Icon(Icons.notifications),
-              onPressed: () {
-                print('Your click notification');
-              }),
-        ],
-      ),
-      body: Center(
-        child: Text("Hello Flutter"),
-      ),
+      home: MyHomePage(),
     );
   }
 }
